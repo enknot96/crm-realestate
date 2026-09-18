0b8724088f59b78130fbe8e928fd627aefb486c2
ENKNOT <256907366+enknot96@users.noreply.github.com>
2026-09-19 07:07:11 +0900

    画像アップロード容量の変更

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      // スマホの写真は1枚数MBになりうる
+      // 巡回報告では最大10枚まとめて送るため、デフォルトの1MBでは足りない
+      bodySizeLimit: "50mb",
+    },
+  },
 };
 
 export default nextConfig;
